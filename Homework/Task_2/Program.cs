b5db76cd7319166fd3acfd9f7495e2dc63e36a52
Prigojev Jurij <136490544+Prigozhev-Yurij@users.noreply.github.com>
2023-06-24 19:35:38 +0300

    CommitTask_2

---
+++ Homework/Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+if (firstDigit == secondDigit)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (secondDigit > firstDigit)
+    {
+        Console.WriteLine("Большее число " + secondDigit);
+        Console.WriteLine("Меньшее число " + firstDigit);
+    }
+    else
+    {
+        Console.WriteLine("Большее число " + firstDigit);
+        Console.WriteLine("Меньшее число " + secondDigit);
+    }
+}
+
