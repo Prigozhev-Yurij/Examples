1a74d44e08e39d45604da989625b83368db267c9
Prigojev Jurij <136490544+Prigozhev-Yurij@users.noreply.github.com>
2023-06-24 20:09:34 +0300

    CommitTask_4

---
+++ Homework/Task_4/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdDigit = Convert.ToInt32(Console.ReadLine());
+if (firstDigit == secondDigit)
+{
+    if (secondDigit == thirdDigit)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    else
+    {
+        if (secondDigit > thirdDigit)
+        {
+            Console.WriteLine("Максимальное число " + secondDigit);
+        }
+        else
+        {
+            Console.WriteLine("Максимальное число " + thirdDigit);
+        }
+    }
+}
+else
+{
+    if (firstDigit > secondDigit)
+    {
+        if (firstDigit > thirdDigit)
+        {
+            Console.WriteLine("Максимальное число " + firstDigit);
+        }
+        else
+        {
+            Console.WriteLine("Максимальное число " + thirdDigit);
+        }
+    }
+    else
+    {
+        if (secondDigit > thirdDigit)
+        {
+            Console.WriteLine("Максимальное число " + secondDigit);
+        }
+        else
+        {
+            Console.WriteLine("Максимальное число " + thirdDigit);
+        }
+    }
+}
